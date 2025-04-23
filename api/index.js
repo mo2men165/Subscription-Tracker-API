1cf38c78d99b2b56f7b89b614f7bbf64d5c36950
mo2men165 <moamenabdeltawab@gmail.com>
2025-04-23 02:34:30 +0200

    index.js

--- api/index.js
+++ api/index.js
@@ -1,3 +1,33 @@
-import app from '../app.js';
+import express from 'express';
+import cookieParser from 'cookie-parser';
+
+import userRouter from '../routes/user.routes.js';
+import authRouter from '../routes/auth.routes.js';
+import subscriptionRouter from '../routes/subscription.routes.js';
+import connectToDatabase from '../database/mongodb.js'
+import errorMiddleware from '../middlewares/error.middleware.js'
+import arcjetMiddleware from '../middlewares/arcjet.middleware.js'
+import workflowRouter from '../routes/workflow.routes.js'
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(arcjetMiddleware);
+
+app.use('/api/v1/auth', authRouter);
+app.use('/api/v1/users', userRouter);
+app.use('/api/v1/subscriptions', subscriptionRouter);
+app.use('/api/v1/workflows', workflowRouter);
+
+app.use(errorMiddleware);
+
+app.get('/', (req, res) => {
+  res.send('Welcome to the Subscription Tracker API!');
+});
+
+  await connectToDatabase();
+
 
 export default app;
