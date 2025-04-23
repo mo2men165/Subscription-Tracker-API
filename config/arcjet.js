edb7324481d81fd323ef5061b6ff92b1e1416d39
mo2men165 <moamenabdeltawab@gmail.com>
2025-04-23 02:49:41 +0200

    tools allowed

--- config/arcjet.js
+++ config/arcjet.js
@@ -8,7 +8,10 @@ const aj = arcjet({
     shield({ mode: "LIVE" }),
     detectBot({
       mode: "LIVE",
-      allow: [ "CATEGORY:SEARCH_ENGINE" ],
+      allow: [ 
+        "CATEGORY:SEARCH_ENGINE",
+        "CATEGORY:TOOL"
+       ],
     }),
     tokenBucket({
       mode: "LIVE",
